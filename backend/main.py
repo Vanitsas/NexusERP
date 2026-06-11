72db3493856e4f9e2cef544ac0fcdab3e1df80fe
Aygun Cerin <sparko249@gmail.com>
2026-06-11 15:18:58 -0700

    Add Netlify URL to CORS

--- backend/main.py
+++ backend/main.py
@@ -14,7 +14,7 @@ app = FastAPI()
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "https://nexuserp-app.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
